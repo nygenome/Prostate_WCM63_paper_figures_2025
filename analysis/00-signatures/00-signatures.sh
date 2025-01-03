6c7c30af539ae4ff345084182f9f006d7382b46b
Will Hooper <whooper@nygenome.org>
2025-01-03 10:35:28 -0500

    Signatures stub

---
+++ analysis/00-signatures/00-signatures.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+################################################################################
+### COPYRIGHT ##################################################################
+
+# New York Genome Center
+
+# SOFTWARE COPYRIGHT NOTICE AGREEMENT
+# This software and its documentation are copyright (2025) by the New York
+# Genome Center. All rights are reserved. This software is supplied without
+# any warranty or guaranteed support whatsoever. The New York Genome Center
+# cannot be responsible for its use, misuse, or functionality.
+
+# Author: Timothy R. Chu
+
+################################################################# /COPYRIGHT ###
+################################################################################
+
+## This bash script contains code associated with the mutational signatures analysis
+set -euo pipefail
+SRCDIR=$(realpath $(dirname $0))
+
+
